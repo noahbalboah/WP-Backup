00aaf76e9842f1d531dd1f5f3192a5b3444cce24
noahbalboah <noah.gregory.weir@gmail.com>
2016-07-27 21:01:16 -0400

    Update wp_backup.sh

--- wp_backup.sh
+++ wp_backup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-logfile="/home/$user/wp_backup.log"
-exec > $logfile 2>&1
+#This is setup for a logfile in the $WEB_ROOT
+LOG_FILE="/home/$user/wp_backup.log"
+exec > $LOG_FILE 2>&1
 
 WEB_ROOT="/home/$user"
 WP_DIR="public_html"
@@ -10,10 +11,9 @@ DB_USER="$DATABASE_USER"
 DB_PWD="$DATABASE_PASSWORD"
 DB_HOST="$HOSTNAME"
 DB="$DATABASE_NAME"
-EMAILED_TO="$EMAIL_ADDRESS_HERE"
+EMAIL="$EMAIL_ADDRESS_HERE"
 
-echo "This is an automated email from your WordPress backup script located at $WEB_ROOT."
-#Backup database to the $WEB_ROOT directory.
+#Backup $DB to the $WEB_ROOT directory.
 cd "$WEB_ROOT"
 mysqldump -h "$DB_HOST" -P 3306 -u "$DB_USER" -p"$DB_PWD" "$DB" > "$DB"-"$DATE".sql
 echo "Database $DB backup complete."
@@ -22,6 +22,6 @@ echo "Database $DB backup complete."
 cd "$WEB_ROOT"
 tar -czf "$WP_DIR"-"$DATE".tgz "$WP_DIR"
 echo "Backup of "$WEB_ROOT"/"$WP_DIR" is complete."
-echo "Backups can be found in your $WEB_ROOT"
 
-/bin/mail -s "WordPress Backup Completed!" $EMAILED_TO <<< "The backup of "$WEB_ROOT"/"$WP_DIR" is now completed. You can find your .sql and .tgz file in $WEB_ROOT. See $logfile for more details."
+#Sends an email to the $EMAIL defined on line #13
+/bin/mail -s "WordPress Backup Completed!" $EMAIL <<< "The backup of "$WEB_ROOT"/"$WP_DIR" is now completed. You can find your .sql and .tgz file in $WEB_ROOT. See $LOG_FILE for more details."
